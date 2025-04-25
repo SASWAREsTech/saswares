b3ffe3b42f49ad056c1cac4c598978688bf881b9
Muhammad Awais Nazir <saswares.tech@gmail.com>
2025-04-26 01:36:03 +0500

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,9 +2,10 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import { FiMenu, FiX } from 'react-icons/fi'
 import DarkModeToggle from './DarkModeToggle'
+import { useEffect, useState, useRef } from 'react'
 
 const navItems = [
   { label: 'Home', href: '/' },
@@ -18,7 +19,30 @@ const Navbar = () => {
   const [scrolled, setScrolled] = useState(false)
   const [menuOpen, setMenuOpen] = useState(false)
   const [activeSection, setActiveSection] = useState('')
+  const menuRef = useRef<HTMLDivElement>(null)
 
+  useEffect(() => {
+    if (!menuOpen) return
+  
+    const handleClickOutside = (e: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        setMenuOpen(false)
+      }
+    }
+  
+    const handleScroll = () => {
+      setMenuOpen(false)
+    }
+  
+    document.addEventListener('mousedown', handleClickOutside)
+    window.addEventListener('scroll', handleScroll)
+  
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [menuOpen])
+  
   useEffect(() => {
     const handleScroll = () => setScrolled(window.scrollY > 10)
     window.addEventListener('scroll', handleScroll)
@@ -50,7 +74,7 @@ const Navbar = () => {
   return (
     <header
       className={`fixed top-0 left-0 right-0 z-50 w-full overflow-x-hidden transition-all duration-300 ${scrolled
-        ? 'backdrop-blur-md bg-white/80 dark:bg-[#0a0a0a]/80 shadow-md'
+        ? ' bg-white/80 dark:bg-[#0a0a0a]/80 shadow-md'
         : 'bg-[#fafafa] dark:bg-[#111]'
         }`}
     >
@@ -100,7 +124,9 @@ const Navbar = () => {
       </nav>
 
       {menuOpen && (
-        <div className="md:hidden fixed inset-x-0 top-[64px] w-full max-w-screen overflow-x-hidden bg-white dark:bg-[#111] px-4 py-6 space-y-4 border-t border-gray-200 dark:border-gray-700 shadow-md z-40 transition-all">
+        <div
+        ref={menuRef} 
+        className="md:hidden fixed inset-x-0 top-[64px] w-full max-w-screen overflow-x-hidden bg-white dark:bg-[#111] px-4 py-6 space-y-4 border-t border-gray-200 dark:border-gray-700 shadow-md z-40 transition-all">
           {navItems.map((item) => (
             <Link
               key={item.href}
